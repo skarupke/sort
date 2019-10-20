5596e2edc815edbba11e2d6168d9b9731f8812fb
Malte Skarupke <malteskarupke@web.de>
2019-10-20 16:08:12 -0400

    Removing unnecessary lines.

--- benchmark/single/memory_usage.h
+++ benchmark/single/memory_usage.h
@@ -9,9 +9,6 @@
 ///         ( See accompanying file LICENSE_1_0.txt or copy at
 ///           http://www.boost.org/LICENSE_1_0.txt )
 ///
-/// @version 0.1
-///
-/// @remarks
 //-----------------------------------------------------------------------------
 
 #include <thread>
